f40d43793ab2cb7dceb604d208d6acba4cae515c
김태연 <tae8753@gmail.com>
2022-08-06 20:13:09 +0900

    2022/08/06 인증방식 변경 (jwt)

---
+++ src/main/java/com/example/demoproject/common/security/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package com.example.demoproject.common.security;
+
+import com.example.demoproject.common.security.entity.UserDetailCustom;
+import com.example.demoproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserService userService;
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return new UserDetailCustom(userService.findByIdWhenLogin(userId));
+    }
+
+    public UserDetailCustom loadUserById(String id){
+        return new UserDetailCustom(userService.findByIdWhenLogin(id));
+    }
+
+    public UserDetailCustom loadUserByUserSeq(long userSeq){
+        return new UserDetailCustom(userService.findById(userSeq));
+    }
+}
